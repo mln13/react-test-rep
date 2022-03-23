999016c8ba58df0eb2eb9932938923077096d4c4
Matheus <kamaitatsu@gmail.com>
2022-03-23 17:16:46 -0300

    first commit

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,36 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import About from '../components/About';
+
+describe('Teste About', () => {
+  it('Testa se há heading 2 com texto About Pokédex', () => {
+    render(<MemoryRouter><About /></MemoryRouter>);
+
+    const aboutHeading2 = screen.getByRole('heading', {
+      level: 2,
+      name: 'About Pokédex',
+    });
+    expect(aboutHeading2).toBeInTheDocument();
+  });
+  it('Testa a página contém as informações sobre a Pokédex', () => {
+    render(<MemoryRouter><About /></MemoryRouter>);
+    const text = screen.getAllByText(/Pokémons/i);
+    text.forEach((param) => expect(param).toBeInTheDocument());
+  });
+  it('Testa se a página contém dois parágrafos', () => {
+    render(<MemoryRouter><About /></MemoryRouter>);
+    const pa = screen.getAllByText(/pokémons/i);
+    expect(pa).toHaveLength(2);
+  });
+
+  it('Testa se a página contém a imagem de uma Pokédex', () => {
+    render(<MemoryRouter><About /></MemoryRouter>);
+
+    const imagem = screen.getByRole('img', {
+      src: 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png',
+    });
+    expect(imagem).toBeInTheDocument();
+    expect(imagem).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,71 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { MemoryRouter, Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import App from '../App';
+
+describe('Teste links fixos', () => {
+  it('Teste se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
+    render(<MemoryRouter><App /></MemoryRouter>);
+
+    const four = 4;
+    const links = screen.getAllByRole('link');
+    const linkHome = screen.getByRole('link', { name: 'Home' });
+    const linkAbout = screen.getByRole('link', { name: 'About' });
+    const linkFavorite = screen.getByRole('link', { name: 'Favorite Pokémons' });
+
+    expect(links).toHaveLength(four);
+    expect(linkHome).toBeInTheDocument();
+    expect(linkAbout).toBeInTheDocument();
+    expect(linkFavorite).toBeInTheDocument();
+  });
+
+  it('Testa se o link Home leva ao /', () => {
+    render(<MemoryRouter><App /></MemoryRouter>);
+    const linkHome = screen.getByRole('link', { name: 'Home' });
+    expect(linkHome).toBeInTheDocument();
+
+    userEvent.click(linkHome);
+    const redirectHome = screen.getByRole('heading', { level: 1, name: 'Pokédex' });
+    expect(redirectHome).toBeInTheDocument();
+  });
+  it('Testa se o link About leva ao /about', () => {
+    render(<MemoryRouter><App /></MemoryRouter>);
+    const linkAbout = screen.getByRole('link', { name: 'About' });
+    expect(linkAbout).toBeInTheDocument();
+
+    userEvent.click(linkAbout);
+    const redirectAbout = screen.getByRole('heading', {
+      level: 2,
+      name: 'About Pokédex',
+    });
+    expect(redirectAbout).toBeInTheDocument();
+  });
+  it('Testa se o link FavoritePokemons leva ao /favorite', () => {
+    render(<MemoryRouter><App /></MemoryRouter>);
+    const linkFavorite = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    expect(linkFavorite).toBeInTheDocument();
+
+    userEvent.click(linkFavorite);
+    const redirectFavorite = screen.getByRole('heading', {
+      level: 2,
+      name: 'Favorite pokémons',
+    });
+    expect(redirectFavorite).toBeInTheDocument();
+  });
+  it('Testa se URL desconhecida leva ao NotFound', () => {
+    const history = createMemoryHistory();
+    render(
+      <Router history={ history }>
+        <App />
+      </Router>,
+    );
+
+    history.push('/pagenotfound');
+    const notFoundLink = screen.getByRole('heading', {
+      level: 2,
+      name: /Page requested not found/i });
+    expect(notFoundLink).toBeInTheDocument();
+  });
+});
